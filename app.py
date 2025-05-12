fc313da994e97bc4d8ca45b0b700146c15c963b2
rionard1256 <riosama1124@gmail.com>
2025-05-13 03:15:45 +0900

    空行消去

--- app.py
+++ app.py
@@ -79,8 +79,6 @@ def delete_subtask(subtask_id):
         db.session.commit()
     return redirect(url_for('index'))
 
-
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
